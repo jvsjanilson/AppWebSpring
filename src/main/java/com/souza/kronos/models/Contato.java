68192880ee1f4b1f14b7bba3aa91d289bef35528
Janilson Varela <janilsonjvs@gmail.com>
2024-04-24 20:04:37 -0300

    limpeza nos imports

--- src/main/java/com/souza/kronos/models/Contato.java
+++ src/main/java/com/souza/kronos/models/Contato.java
@@ -1,8 +1,5 @@
 package com.souza.kronos.models;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
